bb1956ddeb2a6a59ee1eb3b33e4bc9c0e02508ae
Bhanu Priya <bhanupriya1995it@gmail.com>
2022-07-05 08:21:00 +0530

    fourth commit

--- src/bootstrap-component/Carousel/carousel.js
+++ src/bootstrap-component/Carousel/carousel.js
@@ -6,21 +6,21 @@ export default function Carousels() {
         <>
             <Carousel className="carousel-wrapper">
                 <Carousel.Item class="carousel-item">
-                    <img src={require('../../images/wall3.jpg')} alt="item" />
+                    <img src='./Craving/wall3.jpg' alt="item" />
                     <Carousel.Caption className="carousel-caption">
                         <h5>food crave</h5>
                         <p>Feeling hot and bothered? Try it</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item class="carousel-item">
-                    <img src={require('../../images/wall1.jpg')} alt="item" />
+                    <img src='./Craving//wall1.jpg' alt="item" />
                     <Carousel.Caption className="carousel-caption">
                         <h5>food crave</h5>
                         <p>Add some crunch to creamy experience</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item class="carousel-item">
-                    <img src={require('../../images/wall2.jpg')} alt="item" />
+                    <img src='./Craving/wall2.jpg' alt="item" />
                     <Carousel.Caption className="carousel-caption">
                         <h5>food crave</h5>
                         <p>Discover the drinks and desserts</p>
